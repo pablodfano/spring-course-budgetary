1406a1202b87e41269d99faba49092f5dc73ddc3
Pablo Fano <v-pfano@expedia.com>
2018-09-25 12:35:15 -0300

    Initial project add

---
+++ src/main/java/com/pablofano/springcoursebudgetary/SpringCourseBudgetaryApplication.java
@@ -0,0 +1,12 @@
+package com.pablofano.springcoursebudgetary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseBudgetaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCourseBudgetaryApplication.class, args);
+    }
+}
---
+++ src/test/java/com/pablofano/springcoursebudgetary/SpringCourseBudgetaryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pablofano.springcoursebudgetary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCourseBudgetaryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
